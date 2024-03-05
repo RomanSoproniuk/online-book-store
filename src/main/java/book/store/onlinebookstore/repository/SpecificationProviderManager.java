88cdf06fec13f816a0428ec54da1798d9130ec47
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-03-05 18:33:29 +0200

    add criteria API for books

---
+++ src/main/java/book/store/onlinebookstore/repository/SpecificationProviderManager.java
@@ -0,0 +1,4 @@
+package book.store.onlinebookstore.repository;
+
+public interface SpecificationProviderManager {
+}
---
+++ src/main/java/book/store/onlinebookstore/repository/book/AuthorSpecificationProvider.java
@@ -0,0 +1,24 @@
+package book.store.onlinebookstore.repository.book;
+
+import book.store.onlinebookstore.model.Book;
+import book.store.onlinebookstore.repository.SpecificationProvider;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class AuthorSpecification implements SpecificationProvider<Book> {
+    public Specification<Book> getSpecification(String[] params) {
+        return new Specification<Book>() {
+            @Override
+            public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                return root.get("author").in(Arrays.stream(params).toArray());
+            }
+        };
+    }
+}
