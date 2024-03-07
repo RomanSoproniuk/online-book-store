d4a477a2b0b4e9ada4ea7cd38456ae0041fc7998
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-03-07 16:06:16 +0200

    add swagger and pagonation

--- src/main/java/book/store/onlinebookstore/controller/BookController.java
+++ src/main/java/book/store/onlinebookstore/controller/BookController.java
@@ -3,9 +3,12 @@ package book.store.onlinebookstore.controller;
 import book.store.onlinebookstore.dto.BookDto;
 import book.store.onlinebookstore.dto.CreateBookRequestDto;
 import book.store.onlinebookstore.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,33 +20,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book management", description = "Endpoint for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
 
+    @Operation(summary = "Get all books", description = "You can get a certain number of "
+            + "books per issue page and with a certain number of books per page")
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
+    @Operation(summary = "Get book by ID", description = "You can get a specific "
+            + "ID book if one exists")
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.findById(id);
     }
 
+    @Operation(summary = "Add a book to the repository", description = "You can add a specific "
+            + "book with the required parameters to the repository")
     @PostMapping
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         return bookService.save(bookRequestDto);
     }
 
+    @Operation(summary = "Update the book in the repository", description = "You can update a book"
+            + " for a specific ID if one exists")
     @PutMapping("/{id}")
     public BookDto updateBookById(@PathVariable Long id,
                                   @RequestBody @Valid CreateBookRequestDto bookRequestDto) {
         return bookService.update(id, bookRequestDto);
     }
 
+    @Operation(summary = "Remove the book from storage", description = "You can delete a book by "
+            + "a specific ID if it exists in the repository")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
--- src/main/java/book/store/onlinebookstore/service/BookService.java
+++ src/main/java/book/store/onlinebookstore/service/BookService.java
@@ -3,11 +3,12 @@ package book.store.onlinebookstore.service;
 import book.store.onlinebookstore.dto.BookDto;
 import book.store.onlinebookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto bookRequestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
