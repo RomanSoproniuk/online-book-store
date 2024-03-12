911d721783acbf8a290d6f54519865f63fc83f78
Roman Soproniuk <151533079+RomanSoproniuk@users.noreply.github.com>
2024-03-12 10:34:02 +0200

    fix checkstyle

--- src/main/java/book/store/onlinebookstore/exceptions/RegistrationException.java
+++ src/main/java/book/store/onlinebookstore/exceptions/RegistrationException.java
@@ -1,11 +1,11 @@
 package book.store.onlinebookstore.exceptions;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class RegistrationException extends Exception {
     private String message;
+
     public RegistrationException(String message) {
         this.message = message;
     }
